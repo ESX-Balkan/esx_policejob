bd390f55d7d51f12a5a8eddfcfed22068a20797d
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-12-09 19:58:24 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,15 +5,14 @@ Config.MarkerType                 = {Cloakrooms = 20, Armories = 21, BossActions
 Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
 Config.MarkerColor                = {r = 50, g = 50, b = 204}
 
-Config.EnablePlayerManagement     = false -- Enable if you want society managing.
-Config.EnableArmoryManagement     = false
-Config.EnableESXIdentity          = false -- Enable if you're using esx_identity.
-Config.EnableLicenses             = false -- Enable if you're using esx_license.
+Config.EnablePlayerManagement     = true 
+Config.EnableArmoryManagement     = true
+Config.EnableESXIdentity          = true
+Config.EnableLicenses             = true
 
-Config.EnableHandcuffTimer        = true -- Enable handcuff timer? will unrestrain player after the time ends.
+Config.EnableHandcuffTimer        = false -- Enable handcuff timer? will unrestrain player after the time ends.
 Config.HandcuffTimer              = 10 * 60000 -- 10 minutes.
 
-Config.EnableJobBlip              = false -- Enable blips for cops on duty, requires esx_society.
 Config.EnableCustomPeds           = false -- Enable custom peds in cloak room? See Config.CustomPeds below to customize peds.
 
 Config.EnableESXService           = false -- Enable esx service?
@@ -22,9 +21,7 @@ Config.MaxInService               = -1 -- How much people can be in service at o
 Config.Locale                     = 'en'
 
 Config.PoliceStations = {
-
 	LSPD = {
-
 		Blip = {
 			Coords  = vector3(425.1, -979.5, 30.7),
 			Sprite  = 60,
